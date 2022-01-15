bfd05c1d9ed1ef696fe45f34ca6a6bac9ce655fd
AntoniooVictor <86213470+AntoniooVictor@users.noreply.github.com>
2022-01-15 18:07:00 +0700

    style: Format code according to style guide on main.kt

---
+++ main.kt
@@ -0,0 +1,12 @@
+// main.kt - Comprehensive Kotlin starter script
+data class Project(val name: String, val version: String)
+fun main() {
+    val p = Project("GitHub Automated Repository Project", "1.0.0")
+    println("Project: ${p.name}, Version: ${p.version}")
+    println("\nFeatures: Data Classes, Main Function, Loops")
+    for (i in 1..5) {
+        println("  - Iteration $i")
+    }
+}
+
+# Modified on 2022-01-15T00:00:00
