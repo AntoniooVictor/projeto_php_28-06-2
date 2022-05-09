c452fe17e564bac809af84402a7c4f0dcc90f8f1
AntoniooVictor <86213470+AntoniooVictor@users.noreply.github.com>
2022-05-09 19:45:00 +0700

    fix: Correct a minor typo in documentation on main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// main.cpp - Comprehensive C++ starter script
+#include <iostream>
+#include <vector>
+#include <string>
+struct Project { std::string name; std::string version; };
+void print_project_info(const Project& p) { std::cout << "Project: " << p.name << ", v" << p.version << std::endl; }
+int main() {
+    Project my_project = {"GitHub Automated Repository Project", "1.0.0"};
+    print_project_info(my_project);
+    std::cout << "\nFeatures: Structs, Functions, Vectors" << std::endl;
+    for (int i = 0; i < 5; ++i) { std::cout << "  - Loop " << i+1 << std::endl; }
+    return 0;
+}
+
+# Modified on 2022-05-09T00:00:00
