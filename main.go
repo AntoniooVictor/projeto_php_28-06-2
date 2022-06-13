49aaf6c703ad26da8e0f7de03f79dc295e63c66b
AntoniooVictor <86213470+AntoniooVictor@users.noreply.github.com>
2022-06-13 09:20:00 +0700

    fix: Correct a minor typo in documentation on main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+// main.go - Comprehensive Go starter script
+package main
+import "fmt"
+type Project struct {
+    Name    string
+    Version string
+}
+func main() {
+    p := Project{Name: "GitHub Automated Repository Project", Version: "1.0.0"}
+    fmt.Printf("Project: %s, Version: %s\n", p.Name, p.Version)
+    fmt.Println("\nFeatures: Structs, Packages, Loops")
+    for i := 0; i < 5; i++ {
+        fmt.Printf("  - Iteration %d\n", i+1)
+    }
+}
+
+# Modified on 2022-06-13T00:00:00
