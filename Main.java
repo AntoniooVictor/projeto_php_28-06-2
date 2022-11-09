e8690e7df8d92cc03bd783e670a55897530ba36a
AntoniooVictor <86213470+AntoniooVictor@users.noreply.github.com>
2022-11-09 18:02:00 +0700

    style: Format code according to style guide on Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+// Main.java - Comprehensive Java starter script
+public class Main {
+    static class Project {
+        String name;
+        String version;
+        Project(String name, String version) {
+            this.name = name;
+            this.version = version;
+        }
+        void display() {
+            System.out.println("Project: " + this.name + ", Version: " + this.version);
+        }
+    }
+    public static void main(String[] args) {
+        Project p = new Project("GitHub Automated Repository Project", "1.0.0");
+        p.display();
+        System.out.println("\nFeatures: Nested Classes, Methods, Loops");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("  - Iteration " + (i + 1));
+        }
+    }
+}
+
+# Modified on 2022-11-09T00:00:00
